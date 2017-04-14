d8b6d33ca63dd3c6d4d10d20ac08e5cb756a835d
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-14 11:36:37 -0400

    system_prop: Add lte radio props

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,9 @@
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.snapshot_enabled=1 \
+	persist.radio.snapshot_timer=22 \
+	persist.radio.lte_vrte_ltd=1
+
 # Volume
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.vc_call_vol_steps=15 \
