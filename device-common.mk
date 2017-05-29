db0695042853d685ada8ee3654a28a17975fada8
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-28 20:43:21 -0400

    configs: Add media_profiles xml from T560NUUEU1AOK1

--- device-common.mk
+++ device-common.mk
@@ -36,6 +36,10 @@ PRODUCT_PACKAGES += Camera2
 PRODUCT_PACKAGES += \
 	init.target.rc
 
+# Media (camera) configuration files
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml
