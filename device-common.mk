d0a6d1a764b235d8ab61ee93cf2eda2ae56c0693
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-29 20:44:13 -0400

    gte: Make vendor blob inheritance optional
    * Don't terminate a build if vendor blobs are not found

--- device-common.mk
+++ device-common.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 # Inherit from vendor
-$(call inherit-product, vendor/samsung/gte-common/gte-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/gte-common/gte-common-vendor.mk)
 
 LOCAL_PATH := device/samsung/gte-common
 
