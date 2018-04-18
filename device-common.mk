8cc090fd6221e402fb2852d82f8b794f10e86de6
Sean Hoyt <seanhoyt963@gmail.com>
2018-04-18 19:42:04 -0400

    gte-common: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: Ifaf1a19a097e582058ad4f9131843334bf3bfb01

--- device-common.mk
+++ device-common.mk
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
 
 # Media (camera) configuration files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
+	$(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
