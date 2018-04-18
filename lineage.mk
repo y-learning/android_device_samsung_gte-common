bdd32b5109b7429cdfcd5f1d06eb1406983ca392
deadman96385 <seanhoyt963@gmail.com>
2018-04-18 09:26:09 -0500

    Correct vendor location
    Change-Id: I62bb6e6094cc029ae7a4f07e68f7ade66b186878

--- lineage.mk
+++ lineage.mk
@@ -5,8 +5,8 @@ PRODUCT_RELEASE_NAME := Samsung Galaxy Tab E
 TARGET_SCREEN_WIDTH := 800
 TARGET_SCREEN_HEIGHT := 1280
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
