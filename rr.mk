cae456a28d3c1583f64f3989492b509d7c9dfab8
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-31 20:05:56 -0400

    gte-common: rr.mk: cm -> rr
    Change-Id: I1c7abf66b78f87ced23dc0bd82408ab79a89eb04

--- rr.mk
+++ rr.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_WIDTH := 800
 TARGET_SCREEN_HEIGHT := 1280
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/rr/config/common_full_tablet_wifionly.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
